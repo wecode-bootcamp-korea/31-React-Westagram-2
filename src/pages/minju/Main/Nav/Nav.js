bbf5f33af7f5dd2800aaa5e3724687465d6d9c8b
Minju <minju1009@gmail.com>
2022-03-16 21:54:51 +0900

    Modify : Nav scss 수정

--- src/pages/minju/Main/Nav/Nav.js
+++ src/pages/minju/Main/Nav/Nav.js
@@ -12,35 +12,11 @@ function Nav() {
         <div className="navbar__search">
           <input className="navbar__searchBar" placeholder="검색" />
           <i className="fa-solid fa-magnifying-glass searchIcon" />
-          <i className="fa-solid fa-circle-xmark" />
         </div>
         <div className="navbar__moreFunctions">
           <i className="fa-regular fa-compass" />
           <i className="fa-regular fa-heart" />
           <i className="fas fa-regular fa-user-large" />
-        </div>
-
-        <div className="searchToggle">
-          <div className="toggle__arrow" />
-          <div className="searched__items" />
-        </div>
-
-        <div className="overlay" />
-        <div className="profileOptions">
-          <div className="toggle__arrow" />
-          <div className="profileOptions__item">
-            <i className="fa-solid fa-user" />
-            <div className="profileOption__description">프로필</div>
-          </div>
-          <div className="profileOptions__item">
-            <i className="fa-regular fa-bookmark" />
-            <div className="profileOption__description">저장됨</div>
-          </div>
-          <div className="profileOptions__item">
-            <i className="fa-solid fa-gear" />
-            <div className="profileOption__description">설정</div>
-          </div>
-          <div className="profileOptions__item">로그아웃</div>
         </div>
       </div>
     </nav>
