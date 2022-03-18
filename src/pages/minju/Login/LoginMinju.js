b4d4a026ed850341dfa2f235b63fade7ec5596a0
Minju <minju1009@gmail.com>
2022-03-18 17:23:01 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/minju/Login/LoginMinju.js
+++ src/pages/minju/Login/LoginMinju.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginMinju.scss';
 import { Link, useNavigate } from 'react-router-dom';
 
 function LoginMinju() {
+  const [newInput, setNewInput] = useState({
+    id: '',
+    password: '',
+  });
   const navigate = useNavigate();
   const goToMain = () => {
     // 아이디에 @ 포함되어있고, 비밀번호가 5 글자 이상이면
     navigate('/main-minju');
+  };
+
+  const handleInput = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setNewInput(prevValue => ({ ...prevValue, [name]: value }));
   };
 
   return (
@@ -14,12 +24,18 @@ function LoginMinju() {
       <h1 className="logo">Westagram</h1>
       <form action="./main.html" method="get" className="loginForm">
         <input
+          name="id"
+          value={newInput.id}
+          onChange={handleInput}
           className="loginForm__id"
           required
           placeholder="전화번호, 사용자 이름 또는 이메일"
           type="text"
         />
         <input
+          name="password"
+          value={newInput.password}
+          onChange={handleInput}
           className="loginForm__pwd"
           required
           placeholder="비밀번호"
